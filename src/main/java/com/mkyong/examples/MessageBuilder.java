03eda668ea229cd137949cea5f60ee5ab5523c40
Alejandro Gonzalez <33532595+wilson1234567891962@users.noreply.github.com>
2022-10-31 18:32:35 -0500

    Update MessageBuilder.java

--- src/main/java/com/mkyong/examples/MessageBuilder.java
+++ src/main/java/com/mkyong/examples/MessageBuilder.java
@@ -4,17 +4,29 @@ public class MessageBuilder {
 
     public String getMessage(String name) {
 
-        StringBuilder result = new StringBuilder();
-        if (name == null || name.trim().length() == 0) {
+         try {
+             String a = "";
+             AtomicInteger aInt1 = new AtomicInteger(0);
+             AtomicInteger aInt2 = new AtomicInteger(0);
+             if (aInt1.equals(aInt2)) {
+                 System.out.println("Inyecte bug");
+             }
+             StringBuilder result = new StringBuilder();
 
-            result.append("Please provide a name!");
+             if (name == null || name.trim().length() == 0) {
 
-        } else {
-            result.append("");
-            result.append("Hello " + name);
-            result.append("");
-        }
-        return result.toString();
+                 result.append("Please provide a name!");
+
+             } else {
+
+                 result.append("Hello " + name);
+
+             }
+             return result.toString();
+         }catch (Exception e) {
+             e.printStackTrace();
+         }
+         return null;
     }
 
 }
