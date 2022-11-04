1db636e84e7274a0493cef6772fa77c3058e37ce
Wilson Gonzalez <wilson1234567891962@hotmail.com>
2022-11-04 16:33:48 -0500

    Update the code

---
+++ src/main/java/com/mkyong/examples/LeerFicherosExcel.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mkyong.examples;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class LeerFicherosExcel {
+
+	public static void main(String[] args) {
+		String nombreArchivo = "Inventario.xlsx";
+		String rutaArchivo = "C:\\Ficheros-Excel\\" + nombreArchivo;
+		String hoja = "Hoja1";
+
+		try (FileInputStream file = new FileInputStream(new File(rutaArchivo))) {
+			// leer archivo excel
+			XSSFWorkbook worbook = new XSSFWorkbook(file);
+			//obtener la hoja que se va leer
+			XSSFSheet sheet = worbook.getSheetAt(0);
+			//obtener todas las filas de la hoja excel
+			Iterator<Row> rowIterator = sheet.iterator();
+
+			Row row;
+			// se recorre cada fila hasta el final
+			while (rowIterator.hasNext()) {
+				row = rowIterator.next();
+				//se obtiene las celdas por fila
+				Iterator<Cell> cellIterator = row.cellIterator();
+				Cell cell;
+				//se recorre cada celda
+				while (cellIterator.hasNext()) {
+					// se obtiene la celda en específico y se la imprime
+					cell = cellIterator.next();
+					System.out.print(cell.getStringCellValue()+" | ");
+				}
+				System.out.println();
+			}
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	}
+}
--- src/main/java/com/mkyong/examples/MessageBuilder.java
+++ src/main/java/com/mkyong/examples/MessageBuilder.java
@@ -1,32 +1,34 @@
 package com.mkyong.examples;
+
 import java.util.concurrent.atomic.AtomicInteger;
+
 public class MessageBuilder {
 
     public String getMessage(String name) {
 
-         try {
-             String a = "";
-             AtomicInteger aInt1 = new AtomicInteger(0);
-             AtomicInteger aInt2 = new AtomicInteger(0);
-             if (aInt1.equals(aInt2)) {
-                 System.out.println("Inyecte bug");
-             }
-             StringBuilder result = new StringBuilder();
+        try {
+            String a = "";
+            AtomicInteger aInt1 = new AtomicInteger(0);
+            AtomicInteger aInt2 = new AtomicInteger(0);
+            if (aInt1.equals(aInt2)) {
+                System.out.println("Inyecte bug");
+            }
+            StringBuilder result = new StringBuilder();
 
-             if (name == null || name.trim().length() == 0) {
+            if (name == null || name.trim().length() == 0) {
+                String test = "Test";
+                result.append("Please provide a name!");
 
-                 result.append("Please provide a name!");
+            } else {
 
-             } else {
+                result.append("Hello " + name);
 
-                 result.append("Hello " + name);
-
-             }
-             return result.toString();
-         }catch (Exception e) {
-             e.printStackTrace();
-         }
-         return null;
+            }
+            return result.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
