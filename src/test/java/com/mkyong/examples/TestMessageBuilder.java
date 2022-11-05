c748e292da396ef80d9f56be54a34b02f3812c12
Jorge Barrera <ji.barrerar1@uniandes.edu.co>
2022-11-04 20:51:38 -0500

    Fix the next issuehttps://github.com/wilson1234567891962/Excel-Java-Read/issues/112

--- src/test/java/com/mkyong/examples/TestMessageBuilder.java
+++ src/test/java/com/mkyong/examples/TestMessageBuilder.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMessageBuilder {
 
     @Test
-    public void testNameMkyong() {
+    void testNameMkyong() {
 
         MessageBuilder obj = new MessageBuilder();
         assertEquals("Hello mkyong", obj.getMessage("mkyong"));
